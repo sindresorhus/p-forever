1a9e5b2f4153ff48919498da78aacef013ccac38
Dieter Luypaert <dieterluypaert@gmail.com>
2017-06-01 16:04:50 +0200

    fix memory leak

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 'use strict';
 const symbolEnd = Symbol('pForever.end');
 
-module.exports = function loop(fn, prevVal) {
-	return Promise.resolve(prevVal).then(fn).then(x => x !== symbolEnd && loop(fn, x));
+module.exports = function (fn, initVal) {
+	return new Promise((resolve, reject) => {
+		const loop = prevVal => Promise.resolve(prevVal).then(fn).then(lastVal => {
+			if (lastVal === symbolEnd) {
+				return resolve();
+			}
+			loop(lastVal);
+		}).catch(reject);
+		loop(initVal);
+	});
 };
 
 module.exports.end = symbolEnd;
